46076d05a95a7bb5995080fe9109c9ce4ed439d8
Alison <112625415+Alison-Doyle@users.noreply.github.com>
2023-09-02 20:14:11 +0100

    :sparkles: Finished styling mobile view excluding header

--- public/javascript/populateProjects.js
+++ public/javascript/populateProjects.js
@@ -1,5 +1,5 @@
 // Constants
-const projectInformationFilePath = "../resources/json/project_information.json";
+const projectInformationFilePath = "./public/resources/json/project_information.json";
 
 // HTML Elements -> JS ELements
 var projectsContainer = document.getElementById("projectCardsContainer");
@@ -12,7 +12,7 @@ fetch(projectInformationFilePath)
     .then((fileData) => {
         // Creating project card and adding it to index.html
         for (var i = 0; i < fileData.length; i++) {
-            var newCard = `<div class="card"><h3>${fileData[i].project_name}</h3><img src="/public/resources/images/sample.png"><p>${fileData[i].description}</p><div class="cardButtonContainer"><button class="secondaryButton" onclick="window.open('${fileData[i].github_link}', '_blank')">GitHub</button><button class="secondaryButton" onclick="window.open('${fileData[i].live_example_link}', '_blank')">Live Site/App</button></div></div>`;
+            var newCard = `<div class="card"><h3>${fileData[i].project_name}</h3><img src="${fileData[i].image}"><p>${fileData[i].description}</p><div class="cardButtonContainer"><button class="secondaryButton" onclick="window.open('${fileData[i].github_link}', '_blank')">GitHub</button><button class="secondaryButton" onclick="window.open('${fileData[i].live_example_link}', '_blank')">Live Site/App</button></div></div>`;
             projectsContainer.innerHTML += newCard;
         }
     })
