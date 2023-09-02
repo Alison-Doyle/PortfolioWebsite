86abb5c8c91eba91a68bf4231a95820c9e911198
Alison <112625415+Alison-Doyle@users.noreply.github.com>
2023-09-02 16:20:18 +0100

    :sparkles: Added redirect from ./ to ./public/ and other stuff

---
+++ public/javascript/populateProjects.js
@@ -0,0 +1,18 @@
+// Constants
+const projectInformationFilePath = "/public/resources/json/project_information.json";
+
+// HTML Elements -> JS ELements
+var projectsContainer = document.getElementById("projectCardsContainer");
+
+fetch(projectInformationFilePath)
+    .then((result) => {
+        // Getting data from JSON file
+        return result.json();
+    })
+    .then((fileData) => {
+        // Creating project card and adding it to index.html
+        for (var i = 0; i < fileData.length; i++) {
+            var newCard = `<div class="card"><h3>${fileData[i].project_name}</h3><img src="/public/resources/images/sample.png"><p>${fileData[i].description}</p><div class="cardButtonContainer"><button class="secondaryButton" onclick="window.open('${fileData[i].github_link}', '_blank')">GitHub</button><button class="secondaryButton" onclick="window.open('${fileData[i].live_example_link}', '_blank')">Live Site/App</button></div></div>`;
+            projectsContainer.innerHTML += newCard;
+        }
+    })
