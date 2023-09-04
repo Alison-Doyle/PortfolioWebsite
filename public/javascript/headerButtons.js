b7b3a39bd97909da8d3962a92b53ce92ce363cf3
Alison <112625415+Alison-Doyle@users.noreply.github.com>
2023-09-04 18:38:17 +0100

    :sparkles:  Added responsive menu

--- public/javascript/headerButtons.js
+++ public/javascript/headerButtons.js
@@ -1,10 +1,7 @@
 // Importing HTML Elements
 var changeThemeTrigger = document.getElementById("color_mode");
-
-changeThemeTrigger.addEventListener('onclick', () => {
-    console.log("Test")
-    document.body.classList.toggle("darkTheme")
-})
+var menuTrigger = document.getElementById("menu_button");
+var menu = document.getElementById("nav");
 
 changeThemeTrigger.onclick = () => {
     document.body.classList.toggle("darkTheme")
@@ -12,5 +9,15 @@ changeThemeTrigger.onclick = () => {
         changeThemeTrigger.innerHTML = '<span class="material-symbols-rounded">dark_mode</span>'
     } else {
         changeThemeTrigger.innerHTML = '<span class="material-symbols-rounded">light_mode</span>'
+    }
+}
+
+menuTrigger.onclick = () => {
+    if (menu.style.display == 'none') {
+        menu.style.display = 'flex'
+        menuTrigger.innerHTML = '<span class="material-symbols-rounded">close</span>'
+    } else {
+        menu.style.display = 'none'
+        menuTrigger.innerHTML = '<span class="material-symbols-rounded">menu</span>'
     }
 }
